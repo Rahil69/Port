2cead24e2408af3f7eab6bfbeb2159a6e67fbc86
Rahil69 <raahil2005@hotmail.com>
2025-07-16 22:25:17 +0500

    Update: fixed navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     <nav className='min-h-full bg-[#1B1B1B] rounded-b-lg mt-2 mx-2 px-0 py-5 max-w-4xl mx-auto'>
             <div className='flex justify-between items-center'>
               <ul className='flex gap-16 list-none m-0 p-0 pl-20'>
-                  <li><a href="/" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500'>Home </a></li>
-              <li><a href="/about" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500 '>About </a> </li>
-              <li><a href="/contact" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500 '>Contact </a> </li>
-              <li><a href="/contact" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500 '>Projects </a> </li>
+                  <link><a href="/" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500'>Home </a></link>
+              <link><a href="/about" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500 '>About </a> </link>
+              <link><a href="/contact" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500 '>Contact </a> </link>
+              <link><a href="/contact" className='text-gray-400 text-sm font-mono tracking-wide hover:text-white transition-colors duration-500 '>Projects </a> </link>
                       </ul>
                       <div className='flex gap-4 pr-10 text-xl'>
               <a href="https://instagram.com" target='_blank' rel='noopener noreferrer className="text-gray-400 hover:text-white text-4xl'><FaInstagram/> </a>
