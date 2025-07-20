4d5bced33773889f5e95b2f120084cb5e531a6c7
Rahil69 <raahil2005@hotmail.com>
2025-07-20 17:47:01 +0500

    meow

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,10 @@ export default function HomePage() {
                 <section id="home" className="font-mono w-full  flex flex-col justify-center items-center min-h-screen pt-20">
                     <div className="max-w-xl w-full px-4 text-center">
                         <h1 className="text-2xl text-white mb-6">Raahil Mu-umin</h1>
-                        <p className="text-gray-400 mb-6 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste est aspernatur nobis cum sed tenetur dignissimos modi id atque distinctio. Nostrum, earum. Odio, ea quia.</p>
+                        <p className="text-gray-400 mb-6 text-sm">I’m a software developer from Maldives with a keen eye for detail and
+exceptional flexibility. I excel under pressure and consistently deliver
+high-quality results. As a team player, I get along well with everyone
+and am friendly, calm, and adaptable to various work environments</p>
                         <a href="mailto:moominraahil@gmail.com">
                             <button className="bg-[#1B1B1B] hover:bg-[#181818] text-gray-200 font-semibold py-4 px-12 rounded-md text-sm shadow-lg shadow-gray-700/15 transition-all">
                                 contact me
